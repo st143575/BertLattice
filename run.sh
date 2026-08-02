cc17c61c8ef073c490806797a9d79362c0902a87
Chong Shen <st143575@stud.uni-stuttgart.de>
2026-08-02 12:04:20 +0200

    Update run.sh to include command for the optional Gibbs sampling method.

--- run.sh
+++ run.sh
@@ -3,6 +3,16 @@
 pwd
 
 python preprocess.py
+# Uncomment the following command if running closed-set reconstruction (fixed sets of candidate objects & attributes within a domain).
 python get_formal_context.py
+# Uncomment the following command if applying Gibbs sampling according to Definition 9.
+# python get_formal_context.py \
+#   --gibbs_only \
+#   --gibbs_steps 500 \
+#   --gibbs_burn_in 100 \
+#   --gibbs_thinning 5 \
+#   --gibbs_top_k 100 \
+#   --gibbs_temperature 1.0 \
+#   --gibbs_threshold 0.5
 python evaluate.py
 python fca.py
